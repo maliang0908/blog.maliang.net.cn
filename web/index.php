4b79d7ac5ace216e0770877ce5ab8273e75918f0
maliang <maliang@xywy.com>
2017-05-25 11:17:20 +0800

    ml

--- web/index.php
+++ web/index.php
@@ -8,9 +8,6 @@ if(isset($SYSTEM_CONFIG['YII_ENV']) &&  $SYSTEM_CONFIG['YII_ENV']){
     define('YII_ENV', 'dev');
 }
 
-define('YII_DEBUG', true);
-define('YII_ENV', 'dev');
-
 require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
